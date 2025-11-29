398f83c00d93c6b9ea411d5f658802605028cb18
Tuan Hung <>
2025-11-29 12:07:05 +0700

    - Handle cancel token

--- Assets/_Modules/Interactions.Dragging.Extensions/Sample/DragActionSamples/TestExecute.cs
+++ Assets/_Modules/Interactions.Dragging.Extensions/Sample/DragActionSamples/TestExecute.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using Mimi.VisualActions;
@@ -8,13 +9,20 @@ namespace Drag.Sample
     public class TestExecute : MonoBehaviour
     {
         [SerializeField] private VisualAction action;
+
+        private CancellationTokenSource cancellationTokenSource;
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         async UniTask Start()
         {
+            this.cancellationTokenSource = new CancellationTokenSource();
             await action.Initialize();
-            await action.Execute(CancellationToken.None);
+            await action.Execute(this.cancellationTokenSource.Token);
         }
 
-        
+        private void OnDisable()
+        {
+            this.cancellationTokenSource.Cancel();
+        }
     }
 }
